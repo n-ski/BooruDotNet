5b995207ec81b8701f4d4da55a5037ce0f9a9310
n-ski <57114830+n-ski@users.noreply.github.com>
2020-10-25 19:40:35 +0300

    Add unit tests

---
+++ src/BooruDotNet.Tests/Helpers/BooruHelpers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BooruDotNet.Tests.Helpers
+{
+    internal static class BooruHelpers
+    {
+        internal static T Create<T>(Type type)
+        {
+            T booru = (T)Activator.CreateInstance(type);
+            return booru;
+        }
+    }
+}
---
+++ src/BooruDotNet.Tests/Posts.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Threading.Tasks;
+using BooruDotNet.Boorus;
+using BooruDotNet.Tests.Helpers;
+using NUnit.Framework;
+
+namespace BooruDotNet.Tests
+{
+    public class Posts
+    {
+        [Test]
+        [TestCase(typeof(Danbooru), 123456)]
+        [TestCase(typeof(Gelbooru), 5632370)]
+        public async Task GetById_Success(Type booruType, int id)
+        {
+            var booru = BooruHelpers.Create<IBooruPosts>(booruType);
+
+            var post = await booru.GetPostAsync(id);
+
+            Assert.AreEqual(id, post.ID);
+        }
+    }
+}
