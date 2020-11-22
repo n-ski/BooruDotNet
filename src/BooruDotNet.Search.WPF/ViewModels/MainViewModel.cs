7de4f9c48c2163278cf9da7fa43e7d607c7c3f99
n-ski <57114830+n-ski@users.noreply.github.com>
2020-11-22 04:06:20 +0300

    ImageSearch: Add project

---
+++ src/BooruDotNet.Search.WPF/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using ReactiveUI;
+
+namespace BooruDotNet.Search.WPF.ViewModels
+{
+    public class MainViewModel : ReactiveObject
+    {
+        public MainViewModel()
+        {
+        }
+
+        public string SampleText => "Sample Text1111";
+    }
+}
---
+++ src/BooruDotNet.Search.WPF/Views/MainView.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Reactive.Disposables;
+using BooruDotNet.Search.WPF.ViewModels;
+using ReactiveUI;
+
+namespace BooruDotNet.Search.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView : ReactiveWindow<MainViewModel>
+    {
+        public MainView()
+        {
+            InitializeComponent();
+            ViewModel = new MainViewModel();
+
+            this.WhenActivated(d =>
+            {
+                this.OneWayBind(
+                    ViewModel,
+                    vm => vm.SampleText,
+                    v => v.SampleLabel.Content)
+                    .DisposeWith(d);
+            });
+        }
+    }
+}
