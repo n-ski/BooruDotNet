6c0702064f1ff773fe1b88b8c3e1ee213d1b6a29
n-ski <57114830+n-ski@users.noreply.github.com>
2021-02-22 18:58:39 +0300

    Fix tests

--- src/BooruDotNet.Tests/Namers.cs
+++ src/BooruDotNet.Tests/Namers.cs
@@ -35,9 +35,9 @@ namespace BooruDotNet.Tests
         [TestCase(4056776, "__artoria_pendragon_saber_nero_claudius_and_nero_claudius_fate_and_1_more_drawn_by_ishii_wami5285__9b56d855dcbeb438829b1a68a26930b1")]
         [TestCase(4059916, "__nero_claudius_and_nero_claudius_fate_and_1_more_drawn_by_rikui_rella2930__1a09746471cb49c4196ea32135280e63")]
         [TestCase(4060349, "__shishiro_botan_hololive_drawn_by_gfpebs__ec38eb54f588ffcd101e25ad0895bffd")]
-        [TestCase(4060861, "__shishiro_botan_yukihana_lamy_mano_aloe_momosuzu_nene_omaru_polka_and_1_more_hololive_drawn_by_misono_denpachi__002754f8d993c5bf7645a6735abc2307")]
+        [TestCase(4060861, "__shishiro_botan_yukihana_lamy_momosuzu_nene_omaru_polka_mano_aloe_and_1_more_hololive_drawn_by_misono_denpachi__002754f8d993c5bf7645a6735abc2307")]
         [TestCase(4061119, "__original_drawn_by_i_takashi__590cb03c4efec8d13e3fb42c32c38efc")]
-        [TestCase(4067797, "__kurata_mashiro_futaba_tsukushi_yashio_rui_hiromachi_nanami_and_kirigaya_touko_bang_dream_drawn_by_ayasaka__bd4750e801cc84fd2dc1d81ecfd4a3b0")]
+        [TestCase(4067797, "__kurata_mashiro_futaba_tsukushi_yashio_rui_kirigaya_touko_and_hiromachi_nanami_bang_dream_drawn_by_ayasaka__bd4750e801cc84fd2dc1d81ecfd4a3b0")]
         [TestCase(4090626, "__vrchat_drawn_by_kirisame_mia__c903eb2db92678bba106c96b7e71a923")]
         [TestCase(4166623, "__paimon_and_lumine_genshin_impact_drawn_by_ichijiku_suisei__bbc70109203a07c913a2a0f8e418391e")]
         public async Task CreateDanbooruName_Success(int postId, string expectedName)
@@ -58,9 +58,9 @@ namespace BooruDotNet.Tests
         [TestCase(4056776, "artoria pendragon, saber, nero claudius, and nero claudius (fate and 1 more) drawn by ishii_(wami5285) - 9b56d855dcbeb438829b1a68a26930b1")]
         [TestCase(4059916, "nero claudius and nero claudius (fate and 1 more) drawn by rikui_(rella2930) - 1a09746471cb49c4196ea32135280e63")]
         [TestCase(4060349, "shishiro botan (hololive) drawn by gfpebs - ec38eb54f588ffcd101e25ad0895bffd")]
-        [TestCase(4060861, "shishiro botan, yukihana lamy, mano aloe, momosuzu nene, omaru polka, and 1 more (hololive) drawn by misono_denpachi - 002754f8d993c5bf7645a6735abc2307")]
+        [TestCase(4060861, "shishiro botan, yukihana lamy, momosuzu nene, omaru polka, mano aloe, and 1 more (hololive) drawn by misono_denpachi - 002754f8d993c5bf7645a6735abc2307")]
         [TestCase(4061119, "_original drawn by i.takashi - 590cb03c4efec8d13e3fb42c32c38efc")]
-        [TestCase(4067797, "kurata mashiro, futaba tsukushi, yashio rui, hiromachi nanami, and kirigaya touko (bang dream!) drawn by ayasaka - bd4750e801cc84fd2dc1d81ecfd4a3b0")]
+        [TestCase(4067797, "kurata mashiro, futaba tsukushi, yashio rui, kirigaya touko, and hiromachi nanami (bang dream!) drawn by ayasaka - bd4750e801cc84fd2dc1d81ecfd4a3b0")]
         [TestCase(4090626, "_vrchat drawn by kirisame_mia - c903eb2db92678bba106c96b7e71a923")]
         [TestCase(4166623, "paimon and lumine (genshin impact) drawn by ichijiku_suisei - bbc70109203a07c913a2a0f8e418391e")]
         // TODO: case with multiple copyrights but no characters.
